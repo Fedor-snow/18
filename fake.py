82e6fe613d31c1581ac89decac8357a6db3b7599
Fedor-snow <117363957+Fedor-snow@users.noreply.github.com>
2024-11-07 18:33:57 +0300

    Add files via upload

---
+++ fake.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    while second !=0:
+        div = first / second
+        return div
+    else:
+        div = 'Ошибка!'
+        return div
+
+
+# result = divide(first, second)
+# print(result)
---
+++ true.py
@@ -0,0 +1,11 @@
+from math import inf
+
+def divide(first, second):
+    while second !=0:
+        div = first / second
+        return div
+    else:
+        return inf
+
+# result = divide(first, second)
+# print(result)
